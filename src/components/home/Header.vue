b18443fe25d4323754cc972e29c79a9940775932
wwcob6 <wwcob6@gmail.com>
2020-12-08 22:31:33 +0800

    first day

---
+++ src/components/home/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont back-icon">&#xe624;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont search-icon">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe6aa;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    display : flex
+    line-height: .86rem
+    background: #00bcd4;
+    color #fff
+    .header-left
+      width : .64rem
+      float : left
+      .back-icon
+        text-align center
+        font-size 0.4rem
+    .header-right
+      width : 1.24rem
+      float : right
+      text-align center
+    .header-input
+      flex : 1
+      background: #fff
+      line-height .64rem
+      border-radius .1rem
+      height .64rem
+      margin 0.12rem 0 0 0.2rem
+      color #ccc
+      .search-icon
+        padding-left .1rem
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 new Vue({
   router,
   render: h => h(App)
